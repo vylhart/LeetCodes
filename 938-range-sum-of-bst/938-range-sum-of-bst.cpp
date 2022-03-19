f5ffc68a6df40d39375cd5d9c30ef674dab829be
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-03-19 17:22:32 +0530

    Time: 168 ms (55.31%), Space: 64.8 MB (19.40%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -15,8 +15,10 @@ public:
         if(!root)   return 0;
         int sum = 0;
         if(low <= root->val && root->val <= high)   sum+= root->val;
-        sum += rangeSumBST(root->left, low, high);
-        sum += rangeSumBST(root->right,low, high);
+        if(low < root->val)
+            sum += rangeSumBST(root->left, low, high);
+        if(high > root->val)
+            sum += rangeSumBST(root->right,low, high);
         return sum;
     }
 };
