da0d7a388823af784ee7f95a5f038e603dbda299
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-04-03 11:33:19 +0530

    Time: 185 ms (10.88%), Space: 11.4 MB (64.99%) - LeetHub

---
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   
+    int uniqueLetterString(string s) {
+        int ans = 0;
+        int n = s.length();
+        
+        for(char c='A'; c<='Z'; c++){
+            int prev=-1, prevprev=-1;
+            for(int i=0; i<=n; i++){
+                if(i==n && prev!=-1){
+                    ans+= (n-prev)*(prev-prevprev);
+                    //cout<<n-prev;
+                }
+                else if(prev!=-1 && s[i]==c){
+                    ans+= (i-prev)*(prev-prevprev);
+                    prevprev = prev;
+                    prev = i;
+                }
+                else if(s[i]==c){
+                    prevprev = prev;
+                    prev = i;
+                }
+                //cout<<ans<<endl;
+            }
+        }
+        return ans;
+    }
+};
