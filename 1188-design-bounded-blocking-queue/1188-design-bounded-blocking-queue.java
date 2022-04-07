0bff944e787d9c05738c3d050a4ec2946880ac3c
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-04-08 02:24:40 +0530

    Time: 8 ms (82.65%), Space: 42.1 MB (88.49%) - LeetHub

---
+++ 1188-design-bounded-blocking-queue/1188-design-bounded-blocking-queue.java
@@ -0,0 +1,27 @@
+class BoundedBlockingQueue {
+    Semaphore full, empty;
+    Queue<Integer> q;
+    public BoundedBlockingQueue(int capacity) {
+        full = new Semaphore(capacity);
+        empty = new Semaphore(0);
+        q = new LinkedList<>();
+    }
+    
+    public void enqueue(int element) throws InterruptedException {
+        full.acquire();
+        q.add(element);
+        empty.release(1);
+
+    }
+    
+    public int dequeue() throws InterruptedException {
+        empty.acquire();
+        int x = q.remove();
+        full.release();
+        return x;
+    }
+    
+    public int size() {
+        return q.size();
+    }
+}
