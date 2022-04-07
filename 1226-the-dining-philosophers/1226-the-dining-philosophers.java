300d533c5f3363745777b87056c7ce7f1258b4df
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-04-08 01:50:28 +0530

    Time: 20 ms (45.35%), Space: 52.5 MB (13.37%) - LeetHub

---
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -0,0 +1,29 @@
+class DiningPhilosophers {
+    
+    public DiningPhilosophers() {
+        
+    }
+    int i=0;
+    // call the run() method of any runnable to execute its code
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+        synchronized(this){
+            while(philosopher!=i){
+                this.wait();
+            }
+            if(philosopher==i){
+                pickLeftFork.run();
+                pickRightFork.run();
+                eat.run();
+                putRightFork.run();
+                putLeftFork.run();
+                i=(i+1)%5;
+                this.notifyAll();
+            }
+        }
+    }
+}
