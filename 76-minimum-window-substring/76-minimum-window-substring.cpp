83236fac2942975d12e3e7cef901b5ebce704cf2
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-04-19 20:01:13 +0530

    Time: 348 ms (5.33%), Space: 8.3 MB (28.75%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,8 +1,9 @@
+#define MAX 256
 class Solution {
 public:
     
     bool check(vector<int> &v1, vector<int> &v2){
-        for(int i=0; i<256; i++){
+        for(int i=0; i<MAX; i++){
             if(v1[i]>v2[i])    return 0;
         }
         return 1;
@@ -10,15 +11,13 @@ public:
     
     
     string minWindow(string s, string t) {
-        vector<int> freq1(256, 0), freq2(256, 0);
+        vector<int> freq1(MAX, 0), freq2(MAX, 0);
         for(char c: t){
             freq1[c]++;
         }
         
         int n = s.length();
-        int i=0,j=0;
-        int ans = n;
-        int start=0, end=0;
+        int i=0,j=0,ans = n,start=0, end=0;
         while(i<=n){
             
             while(j<i && check(freq1, freq2)){
