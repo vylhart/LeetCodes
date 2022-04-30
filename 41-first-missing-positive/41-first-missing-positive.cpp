dcd934e910cd62d6a6564a04126399712bd06eaa
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-04-30 17:46:55 +0530

    Time: 591 ms (8.25%), Space: 152 MB (6.91%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -4,9 +4,11 @@ public:
         vector<int> nums;
         for(int i:v)    if(i>0) nums.push_back(i);
         unordered_map<long, int> map;
+        if(nums.size()==0)  return 1;
         
         
-        long ans=0, m=nums.size()?nums[0]:0;
+        
+        long ans=0, m=nums[0];
         for(int i:nums){
             if(i<=0)    continue;
             map[i]++;
@@ -15,13 +17,12 @@ public:
         }
         if(m>1) return 1;
         if(ans<=0)  return 1;
-        //cout<<ans;
         for(long i:nums){
             if(i<=0)    continue;
             if(i-1>0 && !map[i-1] && ans>i-1){
                 ans = i-1;
             }
-            if(i+1>0 && !map[(long)i+1] && ans>(long)i+1){
+            if(i+1>0 && !map[i+1] && ans>i+1){
                 ans = i+1;
             }
         }
