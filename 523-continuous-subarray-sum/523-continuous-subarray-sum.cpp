04117e616dd31159e2a5000507a9b2479ed7c5f3
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-05-02 13:18:58 +0530

    Time: 257 ms (37.53%), Space: 96.6 MB (28.94%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -4,14 +4,14 @@ public:
         int n = nums.size();
         unordered_map<int, int> map;
         map[0]=1;
-        bool found=0;
+        int found=0;
         for(int i=0; i<n; i++){
             if(i==0)    nums[i] = nums[i]%k;
             else    nums[i] = (nums[i-1] + nums[i]%k)%k;
             
             found = map[nums[i]];
             if(found){
-                if(i-map[nums[i]]>=0)  return 1;
+                if(i-found>=0)  return 1;
             }
             else  map[nums[i]]=i+2;
         }
