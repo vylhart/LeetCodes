393fce0e24a3ce7278a23996f3e513400291b84e
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-05-26 15:09:59 +0530

    Time: 3 ms (43.98%), Space: 8.5 MB (5.78%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     void find(unordered_map<string,bool> &map, string s, string c, string st, int i, vector<string> &ans){
         if(i==s.length()){
-            if(c==""){
-                ans.push_back(st);
-            }
+            if(c=="")ans.push_back(st);
             return;
         } 
-        c+=s[i];
-        st+=s[i];
+        c+=s[i];st+=s[i];
         find(map, s, c, st, i+1, ans);
         if(map[c]){
-            //cout<<c<<endl;
             if(i!=s.length()-1) st+=" ";
             find(map, s, "", st ,i+1, ans);
         }        
@@ -19,12 +15,9 @@ public:
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         unordered_map<string, bool> map;
-        for(string st: wordDict){
-            map[st]=1;
-        }
+        for(string st: wordDict)map[st]=1;
         vector<string> ans;
         find(map, s, "", "" , 0, ans);
-        //return wordDict;
         return ans;
     }
 };
