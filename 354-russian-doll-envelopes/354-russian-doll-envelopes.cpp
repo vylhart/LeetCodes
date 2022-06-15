c26ded0e7b67ff89acd4111f930457a5fe36f4f6
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-06-16 01:17:30 +0530

    Time: 791 ms (9.90%), Space: 77.5 MB (90.78%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static bool compare(vector<int> &a, vector<int> &b){
+        if(a[0]<b[0])  return 1;
+        else if(a[0]==b[0]) return a[1]>b[1];
+        return 0;
+    }
+    int maxEnvelopes(vector<vector<int>>& e) {
+        sort(e.begin(), e.end(), compare);
+        vector<int> lis;
+        for(auto &i: e){
+            cout<<i[0]<<" "<<i[1]<<endl;
+            int n = lis.size();
+            if(n==0 || (lis[n-1] < i[1]))   lis.push_back(i[1]);
+            else{
+                int j = lower_bound(begin(lis), end(lis), i[1]) - begin(lis);
+                //cout<<j<<" "<<i[1]<<endl;
+                if(j!=n)
+                lis[j] = i[1];
+            }
+        }
+        cout<<endl;
+        return lis.size();
+    }
+};
+
+// 3 4 7 4
