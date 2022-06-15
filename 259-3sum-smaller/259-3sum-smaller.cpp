b7f52c002ddcdc3802e1037293ae02c8a006d251
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-06-15 17:25:15 +0530

    Time: 104 ms (36.30%), Space: 13.6 MB (15.41%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.cpp
+++ 259-3sum-smaller/259-3sum-smaller.cpp
@@ -3,16 +3,13 @@ public:
     
     int countSmaller(vector<int> &v, int l, int h, int t){
         int ans=0;
-        int ll=l;
-        while(l<=h){
-            int m = l+(h-l)/2;
-            if(v[m]<t){
-                ans = m-ll+1;
-                l = m+1;
+        int i=l, j=h;
+        while(i<j){
+            while(i<j && v[i]+v[j]>=t){
+                j--;
             }
-            else{
-                h = m-1;   
-            }
+            ans+= j-i;
+            i++;
         }
         return ans;
     }
@@ -22,9 +19,7 @@ public:
         int n=nums.size();
         int ans=0;
         for(int i=0; i<n-2; i++){
-            for(int j=i+1; j<n-1; j++){
-                ans+= countSmaller(nums, j+1, n-1, target-nums[i]-nums[j]);
-            }
+                ans+= countSmaller(nums, i+1, n-1, target-nums[i]);
         }
         return ans;
     }
