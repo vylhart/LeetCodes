954ce4ba60d65272d227a4f47027a5dab350be4f
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-06-15 23:35:40 +0530

    Time: 4 ms (36.35%), Space: 7.4 MB (41.68%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.cpp
+++ 362-design-hit-counter/362-design-hit-counter.cpp
@@ -1,23 +1,41 @@
 class HitCounter {
-    vector<int> time;
+private:
+    
+    int total;
+    queue<pair<int, int> > hits;
+    
 public:
+    
+    /** Initialize your data structure here. */
     HitCounter() {
-        
+        this->total = 0;
     }
     
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
     void hit(int timestamp) {
-        time.push_back(timestamp);
+        if (this->hits.empty() || this->hits.back().first != timestamp) {
+            // Insert the new timestamp with count = 1
+            this->hits.push({timestamp, 1});
+        } else {
+            // Update the count of latest timestamp by incrementing the count by 1
+            this->hits.back().second++;
+        }
+        this->total++;
     }
     
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
     int getHits(int timestamp) {
-        int i = lower_bound(time.begin(), time.end(), timestamp-300+1) - time.begin();
-        return time.size() - i;
+        while (!this->hits.empty()) {
+            int diff = timestamp - this->hits.front().first;
+            if (diff >= 300) {
+                // Decrement total by the count of the oldest timestamp
+                this->total -= this->hits.front().second;
+                this->hits.pop();
+            }
+            else break;
+        }
+        return this->total;
     }
 };
-
-/**
- * Your HitCounter object will be instantiated and called as such:
- * HitCounter* obj = new HitCounter();
- * obj->hit(timestamp);
- * int param_2 = obj->getHits(timestamp);
- */
