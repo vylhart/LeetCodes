f5718b5d50dab656aa5b7a02c18b369c55f1757b
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-06-22 13:42:23 +0530

    Time: 1696 ms (5.10%), Space: 265.4 MB (5.10%) - LeetHub

---
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
+        vector<long long> dp(n+1, 0);
+        unordered_map<int, vector<pair<int,long long>>> map;
+        for(auto i: rides){
+            map[i[1]].push_back({i[0], i[1]-i[0]+i[2]});
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(auto j: map[i]){
+                dp[i] = max(dp[i], dp[j.first]+j.second);        
+            }
+            dp[i] = max(dp[i], dp[i-1]);
+        }
+        return dp[n];
+        
+    }
+};
