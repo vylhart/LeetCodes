4905531121dd55f5a62f52d1af70bb94b45b523e
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-06-28 03:08:42 +0530

    Time: 1425 ms (12.55%), Space: 10.9 MB (22.34%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -3,10 +3,10 @@ public:
     int threeSumMulti(vector<int>& arr, int target) {
         int n = arr.size();
         int mod = (int) 1e9+7;
-        int ans = 0, i=0;
+        int ans = 0, i=0, t;
         unordered_map<int, int> map;
         for(int i=0; i<n; i++){
-            int t = target - arr[i];
+            t = target - arr[i];
             ans  = (ans + map[t])%mod;
             for(int j=0; j<i; j++){
                 map[arr[i]+arr[j]]++;
