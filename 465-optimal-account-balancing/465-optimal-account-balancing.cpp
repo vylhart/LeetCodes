7fe39c246590dcb76a1e056cf0e94c9e77444afd
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-07-09 19:04:08 +0530

    Time: 214 ms (10.05%), Space: 7 MB (91.12%) - LeetHub

---
+++ 465-optimal-account-balancing/465-optimal-account-balancing.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    vector<int> v;
+public:
+    int find(){
+        int giver = -1;
+        for(int i=0; i<12; i++){
+            if(v[i]>0){
+                giver = i; break;
+            }
+        }
+        if(giver==-1)   return 0;
+        
+        
+        
+        int ans = 100;
+        int money = v[giver];
+        v[giver] = 0;
+        for(int i=0; i<12; i++){
+            if(v[i]<0){
+                v[i]+= money;
+                int x = find();
+                //cout<<giver<<" "<<money<<" "<<x<<" "<<i<<endl;
+                ans = min(ans, x);
+                v[i]-= money;
+            }
+        }
+        v[giver] = money;
+        return ans+1;
+    }
+    
+    int minTransfers(vector<vector<int>>& tran) {
+        int n = tran.size();
+        v.resize(12);
+        for(auto &i: tran){
+            v[i[0]] -= i[2];
+            v[i[1]] += i[2];
+        }
+        return find();
+        
+    }
+};
+
+
+/*
+0 -10 +5   -5
+1 +10      10
+2  -5      -5
+*/
