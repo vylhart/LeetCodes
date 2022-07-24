eceef5e9ffb1287a4efde4f8f713fa39403cb418
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-07-24 13:26:11 +0530

    Time: 290 ms (27.47%), Space: 60.7 MB (16.33%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,22 +1,19 @@
 class UndergroundSystem {
-    unordered_map<int, int> enter;
-    unordered_map<int, string> first; 
+    unordered_map<int, pair<int, string>> enter;
     unordered_map<string, unordered_map<string, int>> total, count;
 public:
     UndergroundSystem() {
         
     }
     
-    void checkIn(int id, string stationName, int t) {
-        enter[id] = t;
-        first[id] = stationName;
+    void checkIn(int id, string f, int t) {
+        enter[id] = {t, f};
     }
     
     void checkOut(int id, string d, int t) {
-        int in = enter[id];
-        string f  = first[id];
-        total[f][d]+= t-in;
-        count[f][d]+= 1;
+        pair<int, string> p = enter[id];
+        total[p.second][d]+= t-p.first;
+        count[p.second][d]+= 1;
     }
     
     double getAverageTime(string f, string d) {
