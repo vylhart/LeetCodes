36c551c13a5e0d1d973307be28c79a8307227260
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-07-30 13:37:50 +0530

    Time: 36 ms (13.05%), Space: 10.9 MB (44.13%) - LeetHub

---
+++ 1086-high-five/1086-high-five.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> highFive(vector<vector<int>>& items) {
+        map<int, priority_queue<int>> map;
+        for(auto i: items){
+            map[i[0]].push(-i[1]);
+            if(map[i[0]].size()>5){
+                map[i[0]].pop();
+            }
+        }
+        vector<vector<int>> ans;
+        for(auto &i: map){
+            int sum=0;
+            int n = i.second.size();
+            while(!i.second.empty()){
+                sum -= i.second.top(); i.second.pop();
+            }
+            ans.push_back({i.first, sum/n});
+        }
+        return ans;
+    }
+};
