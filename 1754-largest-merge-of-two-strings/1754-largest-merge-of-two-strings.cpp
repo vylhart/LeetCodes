c46f7fc3293d6000f32472887b9002c59cac6f57
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-09-06 22:02:10 +0530

    Time: 614 ms (22.75%), Space: 401 MB (23.11%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.cpp
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
-    string largestMerge(string s1, string s2) {
-        if (s1.size() == 0  || s2.size() == 0)
-            return s1 + s2;
-        if (s1 > s2)
-            return s1[0] + largestMerge(s1.substr(1), s2);
-        return s2[0] + largestMerge(s1, s2.substr(1));
+    string largestMerge(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+        string ans = "";
+        int i=0, j=0;
+        while(i<n || j<m){
+            if(i<n && (j==m || word1.substr(i)>=word2.substr(j))){
+                ans += word1[i++];
+            }
+            else{
+                ans += word2[j++];
+            }
+        }
+        return ans;
     }
 };
