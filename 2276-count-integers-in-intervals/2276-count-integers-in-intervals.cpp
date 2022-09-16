a0119fe7303b0cc00146412fb83756462bb10122
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-09-16 18:52:55 +0530

    Time: 1065 ms (19.75%), Space: 186.2 MB (43.37%) - LeetHub

--- 2276-count-integers-in-intervals/2276-count-integers-in-intervals.cpp
+++ 2276-count-integers-in-intervals/2276-count-integers-in-intervals.cpp
@@ -4,26 +4,18 @@ class CountIntervals {
 public:
     CountIntervals() {
         sum=0;
-        //interval[INT_MIN] = INT_MIN;
     }
     
-    bool isOverlap(int l1, int r1, int l2, int r2){
-        return (l1<=l2 && l2<=r1) || (l1<=r2 && r2<=r1) || (l2<=l1 && l1<=r2);
-    }
-    
+
     void add(int left, int right) {
         auto it = interval.lower_bound(left);
-        while(it!=interval.end()){
-            if(it->second <= right){
+        while(it!=interval.end() && it->second <= right){
                 left = min(it->second, left);
                 right= max(it->first, right);
                 sum -= it->first - it->second+1;
                 int tmp = it->first;
                 it++;
                 interval.erase(tmp);
-            }
-            else 
-                break;
         }
         interval[right] = left;
         sum += right-left+1;
