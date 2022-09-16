fae97b4e031ca616d98e6a014b96d3874d2c9d31
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-09-16 18:41:50 +0530

    Time: 1221 ms (10.69%), Space: 186.1 MB (68.93%) - LeetHub

---
+++ 2276-count-integers-in-intervals/2276-count-integers-in-intervals.cpp
@@ -0,0 +1,44 @@
+class CountIntervals {
+    map<int,int> interval;
+    int sum;
+public:
+    CountIntervals() {
+        sum=0;
+        interval[INT_MIN] = INT_MIN;
+    }
+    
+    bool isOverlap(int l1, int r1, int l2, int r2){
+        return (l1<=l2 && l2<=r1) || (l1<=r2 && r2<=r1) || (l2<=l1 && l1<=r2);
+    }
+    
+    void add(int left, int right) {
+        auto it = interval.lower_bound(left);
+        while(it!=interval.end()){
+            if(isOverlap(left, right, it->second, it->first)){
+                left = min(it->second, left);
+                right= max(it->first, right);
+                sum -= it->first - it->second+1;
+                int tmp = it->first;
+                it++;
+                interval.erase(tmp);
+                //cout<<left<<" "<<right<<endl;
+            }
+            else break;
+        }
+        interval[right] = left;
+        sum += right-left+1;
+        
+        //cout<<sum<<endl;
+    }
+    
+    int count() {
+        return sum;
+    }
+};
+
+/**
+ * Your CountIntervals object will be instantiated and called as such:
+ * CountIntervals* obj = new CountIntervals();
+ * obj->add(left,right);
+ * int param_2 = obj->count();
+ */ 
