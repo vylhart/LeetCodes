6cdb2d51b9386e671068474a6ebac7a2575c39b1
Shashank Kumar Gangwar <35990586+vylhart@users.noreply.github.com>
2022-09-18 13:58:00 +0530

    Time: 67 ms (20.94%), Space: 21.3 MB (12.64%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-int countSubstrings(string s) {
-        if(s.length()==0) return 0;
-        int res= 0;
-        for(int i=0; i<s.length(); i++) {
-            expandAroundCenter(s, i, i, res);
-            expandAroundCenter(s, i, i+1, res);
+    int countSubstrings(string s) {
+        int n = s.length();
+        vector<vector<bool>> isPal(n,vector<bool>(n,0));
+        vector<vector<int>>  count(n, vector<int>(n,0));
+        for(int j=0; j<n; j++){
+            for(int i=j; i>=0; i--){
+                if(i==j){
+                    isPal[i][j]=1;
+                    count[i][j]=1;
+                }
+                else if(i+1==j){
+                    isPal[i][j] = s[i]==s[j];
+                    count[i][j] = 2 + isPal[i][j];
+                }
+                else{
+                    isPal[i][j] = s[i]==s[j] && isPal[i+1][j-1];
+                    count[i][j] = isPal[i][j] + count[i+1][j] + count[i][j-1] - count[i+1][j-1];
+                }
+            }
         }
-        return res;
-    }
-    
-    int expandAroundCenter(string s, int left, int right, int& res) {
-        if(s.length()==0 || left>right) return 0;
-        while(left>=0 && right<s.length() && s[left]==s[right]) left--, right++, res++;
-        return res;
+        return count[0][n-1];
     }
 };
